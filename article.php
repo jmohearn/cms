1f50f76beb81be34325331ee829f4f68f6418f8e
Jeremy <jeremy.ohearn1@gmail.com>
2023-11-08 13:35:44 -0500

    dry code, made includes folder, utilized require for reused code

---
+++ article.php
@@ -0,0 +1,43 @@
+<?php
+
+require 'includes/database.php';
+
+if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+    
+    $sql = "SELECT *
+            FROM article
+            WHERE id = " . $_GET['id']; 
+    
+    $results = mysqli_query($conn, $sql);
+    
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $article = mysqli_fetch_assoc($results);
+    
+    }
+} else {
+
+    $article = null;
+}
+
+?>
+
+<?php require 'includes/header.php'; ?>
+            <?php if ($article === null): ?>
+                <p>No articles found.</p>
+
+            <?php else: ?>
+
+            <ul>
+                
+                    <li>
+                        <article>
+                            <h2><?= $article['title']; ?></h2>
+                            <p><?= $article['content']; ?></p>
+                        </article>
+                    </li>
+                
+            </ul>
+            <?php endif; ?>
+<?php require 'includes/footer.php'; ?>
